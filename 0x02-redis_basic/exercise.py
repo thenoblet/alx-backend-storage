9a568dc75b1574064e9e110cb360159a13cc34c1
noblet <appiahpatricknoblet@gmail.com>
2024-08-15 00:54:27 +0100

    added readme

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,11 +21,31 @@ class Cache:
     generated using UUID.
     """
     def __init__(self):
+        """
+        Initializes the Cache class and sets up the Redis connection.
+
+        This method creates a connection to a Redis server and clears
+        the database by calling `flushdb`.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]):
-        random_key = uuid.uuid4()
-        self._redis.set(random_key, data)
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis with a unique key.
+
+        This method generates a unique key using UUID and stores the provided
+        data in Redis associated with this key.
+
+        Parameters:
+        data (Union[str, bytes, int, float]): The data to store in Redis.
+        Can be a string, bytes, integer, or float.
+
+        Returns:
+        uuid.UUID: The unique key associated with the stored data.
+        """
+
+        random_key: str = str(uuid.uuid4())
+        self._redis.set(name=random_key, value=data)
 
         return random_key
