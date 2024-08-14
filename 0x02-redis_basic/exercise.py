38b69f71fad187012e4194670727349ca43513e8
noblet <appiahpatricknoblet@gmail.com>
2024-08-15 00:43:22 +0100

    added readme

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]):
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+
+        return random_key
