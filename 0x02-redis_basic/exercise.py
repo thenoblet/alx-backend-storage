803eb853ff7dea4019163cc2640640494b95138f
noblet <appiahpatricknoblet@gmail.com>
2024-08-15 01:06:21 +0100

    playing with redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,81 @@ The class provides methods for storing data with a unique identifier.
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts the number of times a method is called.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Generate the key using the method's qualified name
+
+        # Increment the count for this key
+        self._redis.incr(key)
+
+        # Call the original method and return its value
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator that stores the history of inputs and outputs for a
+    function in Redis.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_str = str(args)
+
+        self._redis.rpush(method.__qualname__ + ":inputs", input_str)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """
+    Display the history of calls of a particular function.
+
+    Args:
+        method (Callable): The method to replay the call history for.
+    """
+    inputs_key = f"{method.__qualname__}:inputs"
+    outputs_key = f"{method.__qualname__}:outputs"
+
+    redis_client = method.__self__._redis
+
+    inputs = redis_client.lrange(inputs_key, 0, -1)
+    outputs = redis_client.lrange(outputs_key, 0, -1)
+
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+    for input_, output in zip(inputs, outputs):
+        print(
+            f"{method.__qualname__}(*{input_.decode('utf-8')}) "
+            f"-> {output.decode('utf-8')}"
+        )
 
 
 class Cache:
@@ -47,5 +121,47 @@ class Cache:
 
         random_key: str = str(uuid.uuid4())
         self._redis.set(name=random_key, value=data)
-
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """
+        Retrieve the value associated with the given key from Redis and
+        optionally convert it using a provided callable.
+
+        Args:
+            key (str): The key to retrieve the value for.
+            fn (Callable, optional): An optional callable to convert the data
+                back to the desired format. Defaults to None.
+
+        Returns:
+            The retrieved value, optionally converted using `fn`, or
+            None if the key does not exist.
+        """
+        value = self._redis.get(key)
+
+        if value is None:
+            return None
+
+        if fn:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        return self.get(
+            key=key, fn=lambda x: x.decode('utf-8') if x else None
+        )
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieve the value associated with the given key from Redis and
+        convert it to an integer.
+
+        Args:
+            key (str): The key to retrieve the value for.
+
+        Returns:
+            int: The retrieved value as an integer, or None if the key
+            does not exist.
+        """
+        return self.get(key=key, fn=lambda x: int(x) if x else None)
